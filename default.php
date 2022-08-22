dbbf826873e09e92d608da9f22bad7420e0ca415
codewithjanvi <mstryznzmer@gmail.com>
2022-08-22 20:12:52 +0530

    init

---
+++ default.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Google Searching </title>
+</head>
+<body>
+    <form name="searching" action="../cgi-bin/codeExplore.py" method="get">
+        <input type="text" name="txtSearch" id="search" placeholder="Search Here">
+        <input type="submit" value="Search">
+    </form>
+</body>
+</html>
