b8c118bdefee6abaa166241a85585703667c2764
codewithjanvi <mstryznzmer@gmail.com>
2022-08-23 16:31:05 +0530

    some changes in code

--- default.php
+++ default.php
@@ -5,9 +5,11 @@
     <title>Google Searching </title>
 </head>
 <body>
-    <form name="searching" action="../cgi-bin/codeExplore.py" method="get">
+    <form name="searching" method="get" action="codeExplore.py">
         <input type="text" name="txtSearch" id="search" placeholder="Search Here">
         <input type="submit" value="Search">
     </form>
 </body>
 </html>
+
+<!-- action="../cgi-bin/codeExplore.py" -->
