3da94c9899172a8da97dccb7da70cd9bdf3944fa
Zanzmera Jahanvi <83331067+Zanzmera-Jahanvi@users.noreply.github.com>
2022-08-23 16:40:32 +0530

    add url func

--- codeExplore.py
+++ codeExplore.py
@@ -1,20 +1,28 @@
 #!C:\Users\lx\AppData\Local\Programs\Python\Python39\python.exe
 import cgi
-from googlesearch import search 
+from googlesearch import search
+from furl import furl
+
 
 try:
     form = cgi.FieldStorage()
     searchTerm = form.getvalue('txtSearch')
+    fatchURL = furl(f"searchingProject/codeExplore.py/?txtSearch='{searchTerm}'") 
 except:
     print("Something went wrong")
+
+response = fatchURL.args['txtSearch']
 
 print(searchTerm)
 
 
 try:
-    for result in search(searchTerm, tld="co.in",num=10,stop=10,pause=2):
+    for result in search(response, tld="co.in",num=10,stop=10,pause=2):
         print(result)
 except:
     print("something went wrong")
+
+
+
 
 
